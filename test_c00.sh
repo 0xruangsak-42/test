f2ebb7a795d8a3fa32c3cf48c48a0c7324fcd2a6
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-09 17:45:08 +0700

    test case for c00

--- test_c00.sh
+++ test_c00.sh
@@ -1,10 +1,23 @@
 test()
 {
     name=$1
+    result=$2
 
-    echo "****************************************************"
+    echo "***********************************************************************************"
     echo $name
-    echo "****************************************************"
+    echo "***********************************************************************************"
+    echo $result
+    echo "***********************************************************************************"
     ./$name/a.out
-    echo "****************************************************"
+    echo "***********************************************************************************"
 }
+
+test ex00 "character passed as a parameter"
+test ex01 "displays the alphabet in lowercase, on a single line, by ascending order, starting from the letter ’a’"
+test ex02 "displays the alphabet in lowercase, on a single line, by descending order, starting from the letter ’z’"
+test ex03 "displays all digits, on a single line, by ascending order"
+test ex04 "displays ’N’ or ’P’ depending on the integer’s sign entered as a parameter"
+test ex05 "displays all different combinations of three different digits in ascending order"
+test ex06 "displays all different combination of two digits between 00 and 99"
+test ex07 "displays the number entered as a parameter"
+test ex08 "displays all different combinations of n numbers by ascending order"

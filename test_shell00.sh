a0ec03abeb79f8938fbbd8d6dfa5024f3e189e78
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-09 12:18:53 +0700

    a

--- test_shell00.sh
+++ test_shell00.sh
@@ -16,14 +16,14 @@ expect()
     echo result
 }
 test clean
-rm ./ex01/testShell00
-rm -d ./ex02/test0
-rm ./ex02/test1
-rm -d ./ex02/test2
-rm ./ex02/test3
-rm ./ex02/test4
-rm ./ex02/test5
-rm ./ex02/test6
+# rm ./ex01/testShell00
+# rm -d ./ex02/test0
+# rm ./ex02/test1
+# rm -d ./ex02/test2
+# rm ./ex02/test3
+# rm ./ex02/test4
+# rm ./ex02/test5
+# rm ./ex02/test6
 
 test ex00
 expect "Z"
